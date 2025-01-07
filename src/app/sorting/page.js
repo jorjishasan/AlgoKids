c87cfcc15ff28d4c937fa0d748cd812d6d4df58b
jorjishasan <dev.jorjis@gmail.com>
2025-01-08 02:43:20 +0600

    feat: Integrate Toast component into Sorting page for notifications

--- src/app/sorting/page.js
+++ src/app/sorting/page.js
@@ -2,15 +2,17 @@
 import Navigation from '@/components/sorting/Navigation';
 import SortingVisualizer from '@/components/sorting/SortingVisualizer';
 import ComplexityDrawer from '@/components/sorting/ComplexityDrawer';
+import Toast from '@/components/sorting/Toast';
 import { SortingProvider } from '@/context/SortingContext';
 
 const Sorting = () => {
   return (
     <SortingProvider>
-      <div className="min-h-screen bg-gray-900 font-mono">
+      <div className="min-h-screen bg-gray-900 font-sans">
         <Navigation />
         <SortingVisualizer />
         <ComplexityDrawer />
+        <Toast />
       </div>
     </SortingProvider>
   );
