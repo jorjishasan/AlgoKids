dcca34ccc9cf71756e5f974ea5e0f38a8ca64e77
jorjishasan <dev.jorjis@gmail.com>
2025-01-08 02:41:14 +0600

    feat: Enhance AlgorithmSelector with popover for sorting algorithm selection

--- src/components/sorting/AlgorithmSelector.js
+++ src/components/sorting/AlgorithmSelector.js
@@ -1,12 +1,23 @@
-"use client"
+'use client';
+import { useState } from 'react';
+import CaretDown from '@/components/icons/CaretDown';
 
 const AlgorithmSelector = ({ method, setMethod, isRunning }) => {
+    const [showPopover, setShowPopover] = useState(false);
+
   return (
-    <div className="relative group">
-      <button className="text-gray-300 hover:text-white transition-colors">
+    <div   
+    onMouseEnter={() => setShowPopover(true)}
+    onMouseLeave={() => setShowPopover(false)}
+    className="relative">
+      <button
+
+        className="flex items-center gap-1 text-gray-300 hover:text-white transition-colors">
         {method}
+        <CaretDown  />
       </button>
-      <div className="absolute hidden group-hover:block w-48 bg-gray-700 rounded-md shadow-lg">
+      {showPopover && (
+        <div onClick={() => setShowPopover(false)} className="absolute w-48 bg-gray-700 rounded-md ">
         {["Bubble Sort", "Selection Sort", "Merge Sort", "Quick Sort"].map(algo => (
           <button
             key={algo}
@@ -18,6 +29,7 @@ const AlgorithmSelector = ({ method, setMethod, isRunning }) => {
           </button>
         ))}
       </div>
+      )}
     </div>
   );
 };
