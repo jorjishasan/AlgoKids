0cd85d603f954c5ca1f05d4c5bfd9995df71873b
jorjishasan <dev.jorjis@gmail.com>
2025-01-08 05:37:16 +0600

    feat: Introduce dynamic screen size configuration for sorting algorithm settings

--- src/config/algorithmConfig.js
+++ src/config/algorithmConfig.js
@@ -1,3 +1,7 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
 export const sortingAlgorithmConfig = {
   speed: {
     min: 100,
@@ -7,8 +11,8 @@ export const sortingAlgorithmConfig = {
   },
   array: {
     minSize: 2,
-    maxSize: Math.floor(window.screen.width/50),
-    defaultSize: Math.floor((window.screen.width/50)/2)
+    maxSize: 20,
+    defaultSize: 10
   },
   algorithms: [
     "Quick Sort",
@@ -16,4 +20,20 @@ export const sortingAlgorithmConfig = {
     "Selection Sort",
     "Merge Sort"
   ]
+};
+
+export const useScreenSizeConfig = () => {
+  const [sizes, setSizes] = useState({
+    maxSize: sortingAlgorithmConfig.array.maxSize,
+    defaultSize: sortingAlgorithmConfig.array.defaultSize
+  });
+
+  useEffect(() => {
+    setSizes({
+      maxSize: Math.floor(window.screen.width/50),
+      defaultSize: Math.floor((window.screen.width/50)/2)
+    });
+  }, []);
+
+  return sizes;
 }; 
