106a11bc889bbbfc0635efbb611eba9efee8688b
jorjishasan <dev.jorjis@gmail.com>
2025-01-16 04:28:58 +0600

    feat: Add ConvexHullVisualizer component for interactive visualization

---
+++ src/components/convexhull/ConvexHullVisualizer.js
@@ -0,0 +1,40 @@
+"use client"
+import { useConvexHull } from '../../context/ConvexHullContext';
+import Navbar from './Navbar';
+import Canvas from './Canvas';
+
+const ConvexHullVisualizer = () => {
+  const { 
+    points, 
+    hull, 
+    isAnimating, 
+    currentLine,
+    addPoint, 
+    clearPoints,
+    randomizePoints,
+    visualizeConvexHull 
+  } = useConvexHull();
+
+  return (
+    <div className="min-h-screen bg-[#1A1B1E]">
+      <Navbar 
+        isAnimating={isAnimating}
+        points={points}
+        onRandomize={randomizePoints}
+        onVisualize={visualizeConvexHull}
+        onClear={clearPoints}
+      />
+      <div className="mt-8">
+        <Canvas 
+          points={points}
+          hull={hull}
+          currentLine={currentLine}
+          isAnimating={isAnimating}
+          onAddPoint={addPoint}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ConvexHullVisualizer; 
